9e6e8919842b6ad51500cc06961363667209d66f
1139350149 <44402088+1139350149@users.noreply.github.com>
2020-09-09 16:56:53 +0800

    Add files via upload

---
+++ ListStack/ListStack/ListStack.h
@@ -0,0 +1,70 @@
+#pragma once
+#include "LinkNode.h"
+
+template <typename T>
+class ListStack {
+private:
+	LinkeNode<T>* top;
+	int size;
+public:
+	ListStack();
+	ListStack();
+	void clear();
+	bool push(const T item);
+	bool pop(T& item);
+	bool Top(T& item);
+};
+
+template <typename T>
+ListStack<T>::ListStack() {
+	top = nullptr;
+	size = 0;
+}
+
+template <typename T>
+ListStack<T>::ListStack() {
+	clear();
+}
+
+template <typename T>
+void ListStack<T>::clear() {
+	while (top) {
+		LinkeNode<T>* p = top;
+		top = top->next;
+		delete p;
+	}
+	size = 0;
+}
+
+template <typename T>
+bool ListStack<T>::push(const T item) {
+	LinkeNode<T> * p = new LinkeNode<T >(item, top);
+	top = p;
+	size++;
+	return true;
+}
+
+template <typename T>
+bool ListStack<T> ::pop(T& item) {
+	LinkeNode<T>* temp;
+	if (size == 0) {
+		cout << "Stack is Empty!" << endl;
+		return false;
+	}
+	item = top->data;
+	p = top->next;
+	delete top;
+	top = p;
+	size--;
+	return true;
+}
+
+template <typename T>
+bool ListStack<T> ::Top(T& item) {
+	if (size == 0) {
+		item = 0;
+		return false;
+	}
+	item = top->data;
+	return true;
+}
---
+++ ListStack/ListStack/main.cpp
@@ -0,0 +1,5 @@
+#include "ListStack.h"
+
+int main() {
+
+}
